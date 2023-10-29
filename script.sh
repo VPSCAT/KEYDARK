17a022f8d8333cb21dead0dd2f1f48c71c1fa0e6
VPS-CAT <70342707+VPSCAT@users.noreply.github.com>
2023-10-29 17:11:08 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -486,7 +486,7 @@ wget -O $HOME/lista-arq $(ofus "$Key")/$IP >/dev/null 2>&1 && echo -e "\033[1;32
 }
 IP=$(ofus "$Key" | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | grep -o -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}') && echo "$IP" >/usr/bin/venip
 sleep 1s
-function_verify
+#function_verify
 updatedb
 if [[ -e $HOME/lista-arq ]] && [[ ! $(cat $HOME/lista-arq | grep "Code de KEY Invalido!") ]]; then
     msg -bar2
